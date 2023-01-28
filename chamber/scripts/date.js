bc912b730b1e9e8c5e4972e83772b9da7ae067b3
imarenco <ignacio@nyla.app>
2023-01-27 23:50:40 -0300

    w4

---
+++ chamber/scripts/date.js
@@ -0,0 +1,18 @@
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.textContent = fulldate;
+
+
+const catchLastModification = new Date(document.lastModified);
+const yearMonthDay = catchLastModification.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+const hourMinutesSecond = catchLastModification.toLocaleString('en-US', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+const dateTime = `Last Updated: ${yearMonthDay} ${hourMinutesSecond}`;
+document.getElementById("lastDate").innerHTML = dateTime;
+
+// set current year
+document.getElementById("year").innerHTML = `&copy; ${new Date().getFullYear()}`;
+
+
