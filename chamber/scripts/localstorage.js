11aa57e96beedd1056617d10a9b49b0507bad3f3
Ignacio Marenco <ignaciomarenco@Ignacios-MacBook-Air.local>
2023-02-18 02:22:10 -0300

    w7

--- chamber/scripts/localstorage.js
+++ chamber/scripts/localstorage.js
@@ -1,4 +1,13 @@
-const visitNumber = parseInt(window.localStorage.getItem("visits") || 0);
-const visitMessage = `Number of visits: ${visitNumber + 1}`;
+let visitMessage = "This is your first time here";
+
+const visitDate = window.localStorage.getItem("visits");
+const currentDate = new Date();
+
+if (visitDate) {
+    const compareDate = new Date(visitDate);
+    const difTime = currentDate.getTime() - compareDate.getTime();
+    const difDays = difTime / (1000 * 3600 * 24);
+    visitMessage = `You visited us ${parseInt(difDays, 10)} days ago`;
+}
 document.querySelector("#visits").textContent = visitMessage;
-localStorage.setItem("visits", visitNumber + 1);
+localStorage.setItem("visits", currentDate.toString());
