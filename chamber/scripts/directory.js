89ecd498b435f229818267fe50df91f1089c0125
Ignacio Marenco <ignaciomarenco@Ignacios-MacBook-Air.local>
2023-03-06 00:18:17 -0300

    w9

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,43 @@
+const url = './jsons/places.json';
+
+async function getPlaces(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayPlaces(data.places);
+}
+
+getPlaces(url);
+
+const displayPlaces = (places) => {
+    const cards = document.querySelector('div.places');
+
+    places.forEach((place) => {
+        let card = document.createElement('section');
+        let title = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let portrait = document.createElement('img');
+
+        title.textContent = place.name;
+
+     
+
+        phone.textContent = `Phone: ${place.phone}`;
+        address.textContent = `Address:${place.address}`;
+        website.textContent = `${place.website}`;
+        website.setAttribute("href", place.website);
+
+
+        portrait.setAttribute('src', place.img);
+        portrait.setAttribute('alt', `Portait of ${place.name}`);
+        portrait.setAttribute('loading', 'lazy');
+
+        card.appendChild(title);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    })
+}
